0bd7193cd95dcc4bca710a382440f658e8a64c7b
Adam Feng <adamayeah@Adamayeah.local>
2020-12-31 21:19:15 +1100

    changes?

--- apps.js
+++ apps.js
@@ -1,1 +1,10 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const buttons = document.querySelector('.link-button');
 
+    burger.addEventListener("click",()=> {
+        buttons.classList.toggle('nav-active');
+    });
+}
+
+navSlide();
